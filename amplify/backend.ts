0981584fb871d8e54970380f686c4f93125671c7
EC2 Default User <ec2-user@ip-172-31-32-243.eu-west-1.compute.internal>
2025-11-24 17:47:09 +0000

    change bucket names in backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -11,3 +11,10 @@ defineBackend({
   storage, 
   secondaryStorage
 });
+
+// Using a specific bucket names for the storages
+const { cfnBucket: defaultBucket } = backend.storage.resources.cfnResources
+defaultBucket.bucketName = 'consumerspace-tmp-primary'
+
+const { cfnBucket: secondary } = backend.secondaryStorage.resources.cfnResources
+secondry.bucketName = 'consumerspace-tmp-secondary'
