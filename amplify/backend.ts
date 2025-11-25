661bcc57462a7917ff075422593024637ff0d19c
EC2 Default User <ec2-user@ip-172-31-32-243.eu-west-1.compute.internal>
2025-11-25 15:50:10 +0000

    revert

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,50 +1,14 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
-import { Bucket } from "aws-cdk-lib/aws-s3";
-
-// import { storage, secondaryStorage } from './storage/resource';
+import { storage, secondaryStorage } from './storage/resource';
 
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-// defineBackend({
-//   auth,
-//   storage, 
-//   secondaryStorage
-// });
-
-const backend = defineBackend({
-	  auth,
+defineBackend({
+  auth,
+  storage, 
+  secondaryStorage
 });
-
-
-const customBucketStack = backend.createStack("custom_bucket_stack");
-
-// Import existing bucket
-const bucket1 = Bucket.fromBucketAttributes(customBucketStack, "bucket1", {
-  bucketArn: "arn:aws:s3:::consumerspace-temp1",
-  region: "eu-west-1"
-});
-const bucket2 = Bucket.fromBucketAttributes(customBucketStack, "bucket2", {
-  bucketArn: "arn:aws:s3:::consumerspace-temp2",
-  region: "eu-west-1"
-});
-
-backend.addOutput({
-	storage: {
-		buckets: [
-			{
-				aws_region: bucket1.env.region,
-				bucket_name: bucket1.bucketName,
-				name: bucket1.bucketName
-			},
-			{
-				aws_region: bucket2.env.region,
-				bucket_name: bucket2.bucketName,
-				name: bucket2.bucketName
-			}
-		],
-	}
-})
 
